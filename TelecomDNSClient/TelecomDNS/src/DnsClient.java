ba76fad873ad10325c4fd66091e061466eb25269
Snazzythat <roman.andoni@gmail.com>
2017-10-18 17:04:21 -0400

    Primer commit, added first client class

---
+++ TelecomDNSClient/TelecomDNS/src/DnsClient.java
@@ -0,0 +1,10 @@
+/**
+ * Lab 2 Telecom ECSE 489
+ * @author Roman Andoni 260585085
+ * @author Armen Stepanians 260586139
+ *
+ * A simple implementation of a DNS client capable to query A/NS/MX records.
+ */
+public class DnsClient {
+
+}
