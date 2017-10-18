78317dbeb0c25ef52bd5edd0feb45b39a1c70e47
Snazzythat <roman.andoni@gmail.com>
2017-10-18 17:42:00 -0400

    Added usage, added prime objects

--- TelecomDNSClient/TelecomDNS/src/DnsClient.java
+++ TelecomDNSClient/TelecomDNS/src/DnsClient.java
@@ -6,5 +6,56 @@
  * A simple implementation of a DNS client capable to query A/NS/MX records.
  */
 public class DnsClient {
+	
+	private String dnsServerIP;
+	private String domainName;
+	private int timeout;
+	private int retryCount;
+	private int port;
+	private int queryType;
+	
+	public static void main(String[] args) {
+	
+		DnsClient clientIntsance = new DnsClient();
+		
+		if(args.length > 0){
+			try{
+				clientIntsance.parseArguments(args);
+			}
+			catch(Exception e){
+				System.out.println("Failed to parse the arguments. Exiting.");
+				System.exit(69);
+			}
+			
+			clientIntsance.startQuery();
+		}
+		else{
+			System.out.println("No arhuments were passed. See usage:\n");
+			System.out.println("@server | (req) - IPv4 address of the DNSserver");
+			System.out.println("name | (req) - Domain name to query.");
+			System.out.println("-t timeout | (opt) - Timeout before query retransmission");
+			System.out.println("-r max-retries | (opt) - Retry count for retransmission");
+			System.out.println("-p port | (opt) - UDP port number of the DNSserver, default: 53");
+			System.out.print("-mx|-ns  | (opt) - Perform an MX or NS query, default: A");
+			System.exit(69);
+		}
+	}
+	
+	/**
+	 * Parses args
+	 * @param arguments
+	 */
+	public void parseArguments(String[] arguments){
+		
+	}
+	
+	/**
+	 * Start the Query Worker to build the DNS query and send it to the server
+	 */
+	public void startQuery()
+	{
+		
+	}
+	
 
 }
