640861273791e67502ef7d0318b3e9daae1599be
Snazzythat <roman.andoni@gmail.com>
2017-10-18 21:24:51 -0400

    Added DNS request header creation, added QueryRequest class to build the request packet

--- src/DnsQueryRequest.java
+++ src/DnsQueryRequest.java
@@ -1,4 +1,39 @@
-
+/**
+ * Query request class. Responsible to build the DNS packet by joining header and the question.
+ */
 public class DnsQueryRequest {
+
+    private byte[] requestHeader;
+
+    /**
+     * Constructor
+     */
+    public DnsQueryRequest(){
+
+    }
+
+    /**
+     * Gets the header byte array
+     */
+    public void buildDnsRequestHeader(){
+        Header dnsHeader = new Header();
+        requestHeader = dnsHeader.getDNSHeader();
+    }
+
+    /**
+     * Gets the question byte array
+     */
+    public void buildDnsRequestQuestion(){
+
+    }
+
+    /**
+     * Joins header and question for a request packet
+     * And returns the packet data
+     */
+    public void createRequestPacket()
+    {
+
+    }
 
 }
