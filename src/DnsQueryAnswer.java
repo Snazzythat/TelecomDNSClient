e08acb2d496e8178a1f74cde734ebe8f3ee4aa70
Armen Stepanians <armen.stepanians@gmail.com>
2017-10-21 20:40:10 -0400

    Dns answer basic methods and structure

--- src/DnsQueryAnswer.java
+++ src/DnsQueryAnswer.java
@@ -5,11 +5,53 @@ public class DnsQueryAnswer {
 
 	private byte[] dnsQueryAnswer;
 	private int dnsQueryQuestionLength;
+	private int dnsAnswerPointer;
 
 	public DnsQueryAnswer(byte[] dnsQueryAnswer, int dnsQueryQuestionLength) {
 
 		this.dnsQueryAnswer = dnsQueryAnswer;
 		this.dnsQueryQuestionLength = dnsQueryQuestionLength;
+	}
+
+	public void queryAnswer() {
+
+		// TODO: loop the following based on ANCOUNT
+
+		queryAnswerName();
+		queryAnswerType();
+		queryAnswerClass();
+		queryTtl();
+		queryRdLength();
+		queryRData();
+
+	}
+
+	public void queryAnswerName() {
+
+	}
+
+	public void queryAnswerType() {
+
+	}
+
+	public void queryAnswerClass() {
+
+	}
+
+	public void queryTtl() {
+
+	}
+
+	public void queryRdLength() {
+
+	}
+
+	public void queryRData() {
+
+	}
+
+	public boolean isCompression(int offset) {
+		return (dnsQueryAnswer[offset] & 0xC0) == 0xC0;
 	}
 
 	public String queryName(int offset) {
